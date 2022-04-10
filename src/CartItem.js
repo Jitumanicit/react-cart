a50fdd2fd01e88add619c5e9be41a889a4da9b27
Jitumanicit <jitumanicit@gmail.com>
2022-04-10 21:45:34 +0530

    Add event listner

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,33 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 9999,
+            title: 'Redmi 5',
+            qty: 1,
+            img: ''
+        }
+    }
+    increaseQuantity = () => {
+        console.log('this.state', this.state);
+    }
     render(){
+        const {title, price, qty, img} = this.state;
         return(
             <div className ="cart-item">
                 <div className = "left-block">
                     <img style={styles.image} />
                 </div>
                 <div className = "right-block">
-                    <div style={{color:'red'}}>Phone</div>
-                    <div>Rs 999</div>
-                    <div>Uty : 1</div>
+                    <div style={{color:'red'}}>{title}</div>
+                    <div>Rs {price}</div>
+                    <div>Qty : {qty}</div>
                     <div className="cart-item-action">
-                        {/* {Buttons} */}
+                        <img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2569/premium/2569198.png?token=exp=1649605794~hmac=eb374de2eb04e0d7ea8fd61261191be6" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" />
                     </div>
                 </div>
             </div>
