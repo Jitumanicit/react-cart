7356f293aa7da50ab2d3b9b879a42af9a7cc06fd
Jitumanicit <jitumanicit@gmail.com>
2022-04-14 15:19:15 +0530

    Add Total count and navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,93 @@
 import React from 'react';
-import CartItem from './CartItem';
+import Cart from './Cart';
+import Navbar from './Navbar';
 
-
-function App() {
-  return (
-    <div className="App">
-      <CartItem />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          products: [
+              {
+                  price: 99,
+                  title: 'Watch',
+                  qty: 1,
+                  img: 'https://cdn-icons.flaticon.com/png/512/2912/premium/2912536.png?token=exp=1649928688~hmac=43276b29b4f1454782d76835671c085e',
+                  id: 1
+              },
+              {
+                  price: 999,
+                  title: 'Mobile Phone',
+                  qty: 10,
+                  img: 'https://cdn-icons.flaticon.com/png/512/2586/premium/2586488.png?token=exp=1649928091~hmac=2331e3847f74db75e0b04029131e39d9',
+                  id: 2
+              },
+              {
+                  price: 999,
+                  title: 'Laptop',
+                  qty: 4,
+                  img: 'https://cdn-icons-png.flaticon.com/512/610/610021.png',
+                  id: 3
+              }
+          ]
+      }
+  }
+  handleIncreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+      products[index].qty +=1;
+      this.setState({
+          products: products
+      })
+  }
+  handleDecreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+      if(products[index].qty === 1){
+          return;
+      }
+      products[index].qty -=1;
+      this.setState({
+          products: products
+      })
+  }
+  handleDeleteProduct = (id) => {
+      const {products} = this.state;
+      const items = products.filter((item) => item.id !== id);
+      this.setState({
+          products: items
+      })
+  }
+  getCartCount = () => {
+    const {products} = this.state;
+    let count = 0;
+    products.forEach((product) => {
+      count += product.qty;
+    })
+    return count;
+  }
+  getCartTotal = () => {
+    const {products} = this.state;
+    let cartTotal = 0;
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price;
+    })
+    return cartTotal;
+  } 
+  render(){
+    const {products} = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()}/>
+        <Cart 
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div>Total: {this.getCartTotal()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,54 +1,30 @@
 import React from "react";
 
-class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 9999,
-            title: 'Redmi 5',
-            qty: 1,
-            img: ''
-        }
-    }
-    increaseQuantity = () => {
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // })
-
-        // if previous state is required then used this
-        this.setState((prevState) => {
-            return{
-                qty: prevState.qty + 1
-            }
-        })
-    }
-    decreaseQuantity = ()=> {
-        this.setState((prevState) => {
-            return{
-                qty: prevState.qty - 1
-            }
-        })
-    }
-    render(){
-        const {title, price, qty, img} = this.state;
-        return(
-            <div className ="cart-item">
-                <div className = "left-block">
-                    <img style={styles.image} />
-                </div>
-                <div className = "right-block">
-                    <div style={{color:'red'}}>{title}</div>
-                    <div>Rs {price}</div>
-                    <div>Qty : {qty}</div>
-                    <div className="cart-item-action">
-                        <img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png" />
-                        <img alt="decrease" onClick={this.decreaseQuantity} className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2569/premium/2569198.png?token=exp=1649605794~hmac=eb374de2eb04e0d7ea8fd61261191be6" />
-                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" />
-                    </div>
+const CartItem = (props) => {
+    const {title, price, qty, img} = props.product;
+    const {
+        product,
+        onIncreaseQuantity,
+        onDecreaseQuantity,
+        onDeleteProduct
+    } = props;
+    return(
+        <div className ="cart-item">
+            <div className = "left-block">
+                <img style={styles.image} src={product.img} />
+            </div>
+            <div className = "right-block">
+                <div style={{color:'red'}}>{title}</div>
+                <div>Rs {price}</div>
+                <div>Qty : {qty}</div>
+                <div className="cart-item-action">
+                    <img alt="increase" onClick={()=> onIncreaseQuantity(product)} className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png" />
+                    <img alt="decrease" onClick={()=> onDecreaseQuantity(product)} className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2569/premium/2569198.png?token=exp=1649605794~hmac=eb374de2eb04e0d7ea8fd61261191be6" />
+                    <img alt="delete" onClick={()=> onDeleteProduct(product.id)} className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 const styles = {
